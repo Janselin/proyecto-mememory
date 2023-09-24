07b56263de32b06df6b0590e60e6d0c2c275846c
MaryF <maryanfer23@gmail.com>
2023-09-24 19:58:54 -0300

    js base para cambio de avatars en login

---
+++ js/avatars.js
@@ -0,0 +1,96 @@
+
+// Cambio de avatars
+
+const avatar_logo = document.querySelector("#avatar-logo")
+const avatar_user = document.querySelector("#avatar-user");
+const avatar_forgot = document.querySelector("#avatar-forgot");
+const avatar_pass = document.querySelector("#avatar-pass");
+const botton_forgot  = document.querySelector(".link-contraseña");
+
+
+const form = document.querySelector('.formulario');
+const inputMail = document.querySelector("#usuario");
+const inputPass = document.querySelector("#contraseña");
+
+
+
+inputMail.addEventListener('keyup', (e) => {
+
+   change_img(avatar_logo,avatar_user);
+   avatar_pass.style="display: none";
+})
+
+
+inputPass.addEventListener('keyup', (e) => {
+
+   change_img(avatar_logo,avatar_pass);
+   avatar_user.style="display: none";
+   
+})
+
+
+
+function change_img(avatar,img) {
+
+      avatar.style="display: none";
+      img.style="display:block";
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// hover 
+botton_forgot.addEventListener("mouseover", (e) => {
+   console.log("hola");
+
+   avatar_logo.style="display: none";
+   avatar_user.style="display: none";
+   avatar_pass.style="display: none";
+   avatar_forgot.style="display:block";
+
+
+})
+
+
+botton_forgot.addEventListener("mouseout", (e) => {
+   console.log("hola");
+   avatar_logo.style="display:block";
+   avatar_forgot.style="display: none";
+
+})
+
+
