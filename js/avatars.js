b88507f46c3c91fcbeb605e8ea662b0c36102de8
MaryF <maryanfer23@gmail.com>
2023-09-24 21:03:50 -0300

    code base

--- js/avatars.js
+++ js/avatars.js
@@ -6,11 +6,13 @@ const avatar_user = document.querySelector("#avatar-user");
 const avatar_forgot = document.querySelector("#avatar-forgot");
 const avatar_pass = document.querySelector("#avatar-pass");
 const botton_forgot  = document.querySelector(".link-contraseña");
+const avatar_happy = document.querySelector('.avatar-happy')
 
 
 const form = document.querySelector('.formulario');
 const inputMail = document.querySelector("#usuario");
 const inputPass = document.querySelector("#contraseña");
+const boton_iniciar = document.querySelector(".login")
 
 
 
@@ -42,44 +44,14 @@ function change_img(avatar,img) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// hover 
+// hover de password
 botton_forgot.addEventListener("mouseover", (e) => {
    console.log("hola");
 
    avatar_logo.style="display: none";
    avatar_user.style="display: none";
    avatar_pass.style="display: none";
+   
    avatar_forgot.style="display:block";
 
 
@@ -94,3 +66,14 @@ botton_forgot.addEventListener("mouseout", (e) => {
 })
 
 
+// boton_iniciar.addEventListener("mouseover", (e) => {
+//    console.log("hola");
+
+//    avatar_logo.style="display: none";
+//    avatar_user.style="display: none";
+//    avatar_pass.style="display: none";
+   
+//    avatar_happy.style="display:block";
+
+
+// })
