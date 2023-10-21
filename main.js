a29171d07b56db94ba7ec18b665c9fbfd99ec5c9
Mary F <maryanfer23@gmail.com>
2023-10-20 23:24:08 -0300

    se oculto datos de firebase

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 navigator.serviceWorker.register('sw.js');
+
+
+let initializeApp;
+
+fetch('/__/firebase/init.json').then(async response => {
+  const config = await response.json();
+  initializeApp = initializeApp(config);
+})
+
+export const app = initializeApp;
