143df4cb650cee4c1da4d19c5f40008b52ce6bf4
Mary F <maryanfer23@gmail.com>
2023-10-21 20:39:31 -0300

    se oculta el boton empezar al darle click

--- js/star.js
+++ js/star.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (juego.empezar) {
         juego.empezar.addEventListener("click", function () {
             juego.desbloquear();
+            empezar.style="display:none";           
             juego.buttons.forEach((button, index) => {
                 button.addEventListener("click", () => {
                     juego.destapar(index);
