e9e8bfdda59d1f92413e5a14a734d86d62e7a8ff
Mary F <maryanfer23@gmail.com>
2023-10-21 20:02:51 -0300

    se borro datos de firebase por el momento

--- main.js
+++ main.js
@@ -1,12 +1,2 @@
 // service worker 
 navigator.serviceWorker.register('sw.js');
-
-// datos de firebase ocultos
-let initializeApp;
-
-fetch('/__/firebase/init.json').then(async response => {
-  const config = await response.json();
-  initializeApp = initializeApp(config);
-})
-
-export const app = initializeApp;
