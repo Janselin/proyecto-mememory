e9a5e34e3d7af900d0e31e82eed823fa196b8ae9
Mary F <maryanfer23@gmail.com>
2023-10-21 22:32:05 -0300

    estilos de boton empezar

--- js/star.js
+++ js/star.js
@@ -1,12 +1,13 @@
 import * as juego from './juego.js';
 
+
 juego.iniciarJuego();
 
 document.addEventListener("DOMContentLoaded", function () {
     if (juego.empezar) {
         juego.empezar.addEventListener("click", function () {
-            juego.desbloquear();
-            empezar.style="display:none";           
+            juego.desbloquear();            
+            empezar.style="display:none";                   
             juego.buttons.forEach((button, index) => {
                 button.addEventListener("click", () => {
                     juego.destapar(index);
