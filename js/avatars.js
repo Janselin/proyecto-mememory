4e44b3c1353f42f2771f7ac97198bff59b52c836
Mary F <maryanfer23@gmail.com>
2023-10-25 00:11:50 -0300

    added avatar-happy + inputPass focusout

--- js/avatars.js
+++ js/avatars.js
@@ -5,7 +5,9 @@ const avatar_logo = document.querySelector("#avatar-logo")
 const avatar_user = document.querySelector("#avatar-user");
 const avatar_forgot = document.querySelector("#avatar-forgot");
 const avatar_pass = document.querySelector("#avatar-pass");
+const avatar_happy = document.querySelector("#avatar-happy");
 const botton_forgot  = document.querySelector(".link-contraseña");
+
 const inputMail = document.querySelector("#usuario");
 const inputPass = document.querySelector("#contraseña");
 
@@ -27,7 +29,12 @@ inputPass.addEventListener('keyup', (e) => {
    
 });
 
+inputPass.addEventListener('focusout', (e) => {
 
+   change_img(avatar_pass,avatar_happy);
+   avatar_user.style="display: none";
+   
+});
 
 function change_img(avatar,img) {
 
@@ -43,6 +50,7 @@ botton_forgot.addEventListener("mouseover", (e) => {
    avatar_logo.style="display: none";
    avatar_user.style="display: none";
    avatar_pass.style="display: none";   
+   avatar_happy.style="display: none";   
    avatar_forgot.style="display:block";
 
 
